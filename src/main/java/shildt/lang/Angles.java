5183fbe42238f26a9c00d5b49f894fe28491e9b4
Marat Mukhamadeew <marat.mukhamadeew@yandex.ru>
2023-12-01 13:21:20 +0500

    Commited 01.12.2023

---
+++ src/main/java/shildt/lang/Angles.java
@@ -0,0 +1,14 @@
+package shildt.lang;
+
+// Продемонстрировать применение методов toDegrees() и toRadiant()
+
+public class Angles {
+    public static void main(String[] args) {
+        double theta = 120.0;
+
+        System.out.println(theta + " градусов равно " + Math.toRadians(theta) + " радиан.");
+
+        theta = 1.312;
+        System.out.println(theta + " радиан равно " + Math.toDegrees(theta) + " градусов");
+    }
+}
